efef9d1c751ea5ea052b6973d3a9b147e37f72a2
kacper29 <niemam29@gmail.com>
2022-11-09 15:19:12 +0100

    Fixed cr and prettier

--- test/NotIERCBorrowerTests.ts
+++ test/NotIERCBorrowerTests.ts
@@ -3,29 +3,29 @@ import { ethers } from 'hardhat'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { Contract } from 'ethers'
 let owner: SignerWithAddress,
-  user: SignerWithAddress,
-  FlashLoanContract,
-  InvalidAllowanceBorrowerContract,
-  InvalidAllowanceBorrower: Contract,
-  FlashLoan: Contract,
-  TokenAContract,
-  TokenA: Contract
+    user: SignerWithAddress,
+    FlashLoanContract,
+    InvalidAllowanceBorrowerContract,
+    InvalidAllowanceBorrower: Contract,
+    FlashLoan: Contract,
+    TokenAContract,
+    TokenA: Contract
 
 describe('Borrowing Loans - invalid interface tests', function () {
-  before(async function () {
-    ;[owner, user] = await ethers.getSigners()
+    before(async function () {
+        ;[owner, user] = await ethers.getSigners()
 
-    FlashLoanContract = await ethers.getContractFactory('FlashLoan')
-    InvalidAllowanceBorrowerContract = await ethers.getContractFactory('NotIERCBorrower')
-    TokenAContract = await ethers.getContractFactory('TokenA')
-    TokenA = await TokenAContract.deploy('TokenA', 'TKA', 18)
-    FlashLoan = await FlashLoanContract.deploy(TokenA.address)
-    InvalidAllowanceBorrower = await InvalidAllowanceBorrowerContract.deploy(FlashLoan.address)
-    await TokenA.transfer(FlashLoan.address, 1000000)
-    await TokenA.approve(FlashLoan.address, 1000000)
-    await TokenA.transfer(InvalidAllowanceBorrower.address, 3010000)
-  })
-  it('Should revert the transaction when borrower is not recognized', async function () {
-    await expect(InvalidAllowanceBorrower.borrow(TokenA.address, 1000000)).to.be.reverted
-  })
+        FlashLoanContract = await ethers.getContractFactory('FlashLoan')
+        InvalidAllowanceBorrowerContract = await ethers.getContractFactory('NotIERCBorrower')
+        TokenAContract = await ethers.getContractFactory('TokenA')
+        TokenA = await TokenAContract.deploy('TokenA', 'TKA', 18)
+        FlashLoan = await FlashLoanContract.deploy(TokenA.address)
+        InvalidAllowanceBorrower = await InvalidAllowanceBorrowerContract.deploy(FlashLoan.address)
+        await TokenA.transfer(FlashLoan.address, 1000000)
+        await TokenA.approve(FlashLoan.address, 1000000)
+        await TokenA.transfer(InvalidAllowanceBorrower.address, 3010000)
+    })
+    it('Should revert the transaction when borrower is not recognized', async function () {
+        await expect(InvalidAllowanceBorrower.borrow(TokenA.address, 1000000)).to.be.reverted
+    })
 })
