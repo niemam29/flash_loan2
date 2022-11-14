18ed1ba8f55ec180aec0d62a4295969e83e9ad35
kacper29 <niemam29@gmail.com>
2022-11-14 13:49:36 +0100

    Deleted not used code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,11 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
-    solidity: '0.8.17',
-    networks: {
-        hardhat: { allowUnlimitedContractSize: true },
-        localhost: { allowUnlimitedContractSize: true },
-    },
+  solidity: '0.8.17'
 }
 
 export default config
